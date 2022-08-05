4c86ea1ce80a25767cc225fdea6c6945e3b54f04
eduardo <eduardo.carneiro2907@gmail.com>
2022-08-05 14:34:39 -0300

    Add main.rb and lib/ files

---
+++ lib/knight.rb
@@ -0,0 +1,5 @@
+class Knight
+  def initialize(board)
+    @board = board
+  end
+end
---
+++ main.rb
@@ -0,0 +1,5 @@
+require_relative 'lib/board'
+require_relative 'lib/knight'
+
+board = Board.new
+knight = Knight.new(board)
