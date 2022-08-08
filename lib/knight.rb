ddf5e222cba3f65e6eb276a15f36d3ab0d425a77
eduardo <eduardo.carneiro2907@gmail.com>
2022-08-08 20:23:36 -0300

    Fix invalid coordinates being passing to MoveTree class

--- lib/knight.rb
+++ lib/knight.rb
@@ -6,9 +6,15 @@ class Knight
   end
 
   def move(initial_pos, target_pos)
-    moves_tree = MoveTree.new(initial_pos, @board)
-    path = moves_tree.find_path(target_pos)
-    puts "You made it in #{path.size} moves! Here's your path:"
-    path.each { |node| p node.coordinate }
+    if !(@board.coordinate_in_board?(initial_pos) && @board.coordinate_in_board?(target_pos))
+      puts "Coordinates out of range! Be sure that the coordinates are between #{@board.x_axis}x#{@board.y_axis} board."
+    elsif initial_pos == target_pos
+      puts 'Coordinates are the same! Try again.'
+    else
+      moves_tree = MoveTree.new(initial_pos, @board)
+      path = moves_tree.find_path(target_pos)
+      puts "You made it in #{path.size} moves! Here's your path:"
+      path.each { |node| p node.coordinate }
+    end
   end
 end
